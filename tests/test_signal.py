47626be40d440da18edfda4d0d5a6f6c23dc20ba
Richard <richard.upwork.awj5R@protonmail.com>
2023-07-27 08:33:11 -0400

    Update test_signal.py

--- tests/test_signal.py
+++ tests/test_signal.py
@@ -1,13 +1,50 @@
 import pytest
-from calculators.signal_calculator import SignalCalculator
+from your_module import SignalRules
 
+@pytest.fixture
+def signal_rules_instance():
+    # Create an instance of SignalRules for testing
+    return SignalRules()
 
-@pytest.mark.parametrize("adx_green, adx_red, rsi, expected_signal", [
-    (1.5, 1.2, 25, "Long"),
-    (0.8, 1.2, 75, "Short"),
-    (1.3, 1.1, 40, "No Signal"),
-    (1.2, 1.3, 70, "No Signal"),
+@pytest.mark.parametrize("adx_green, adx_red, rsi, expected_result", [
+    # Test cases for check_long_signal method
+    (20, 15, 35, False),  # Signal rules not satisfied
+    (25, 20, 25, True),   # Signal rules satisfied
+    # Add more test cases as needed
 ])
-def test_generate_signal(adx_green, adx_red, rsi, expected_signal):
-    signal = SignalCalculator.generate_signal(adx_green, adx_red, rsi)
-    assert signal == expected_signal
+def test_check_long_signal(signal_rules_instance, adx_green, adx_red, rsi, expected_result):
+    assert signal_rules_instance.check_long_signal(adx_green, adx_red, rsi) == expected_result
+
+@pytest.mark.parametrize("adx_green, adx_red, rsi, expected_result", [
+    # Test cases for check_short_signal method
+    (20, 25, 60, False),  # Signal rules not satisfied
+    (30, 35, 75, True),   # Signal rules satisfied
+    # Add more test cases as needed
+])
+def test_check_short_signal(signal_rules_instance, adx_green, adx_red, rsi, expected_result):
+    assert signal_rules_instance.check_short_signal(adx_green, adx_red, rsi) == expected_result
+
+def test_reset(signal_rules_instance):
+    # Test the reset method
+
+    # Set some initial values to True
+    signal_rules_instance.adx_green_over_red = True
+    signal_rules_instance.adx_red_over_green = True
+    signal_rules_instance.rsi_below_30 = True
+    signal_rules_instance.rsi_above_30 = True
+    signal_rules_instance.rsi_above_70 = True
+    signal_rules_instance.rsi_below_70 = True
+
+    # Call the reset method
+    signal_rules_instance.reset()
+
+    # Check if all values are reset to False
+    assert not signal_rules_instance.adx_green_over_red
+    assert not signal_rules_instance.adx_red_over_green
+    assert not signal_rules_instance.rsi_below_30
+    assert not signal_rules_instance.rsi_above_30
+    assert not signal_rules_instance.rsi_above_70
+    assert not signal_rules_instance.rsi_below_70
+
+    # Add more reset test cases as needed
+
