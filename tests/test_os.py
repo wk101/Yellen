616a5bdf4a132cfe699d7c6984297e5d1f6fc07e
Richard <richard.upwork.awj5R@protonmail.com>
2023-07-27 08:31:02 -0400

    Update test_os.py

--- tests/test_os.py
+++ tests/test_os.py
@@ -1,16 +1,25 @@
 import pytest
-from calculators.order_size import OrderSize
-
+from your_module import OrderSize
 
 @pytest.fixture
-def order_size() -> OrderSize:
-    return OrderSize(initial_size=1.0)
+def order_size_instance():
+    # Create an instance of OrderSize for testing
+    initial_size = 100000  # Change this value as needed
+    lot_size = 100000      # Change this value as needed
+    return OrderSize(initial_size, lot_size)
 
+def test_calculate_size(order_size_instance):
+    # Test the calculate_size method for different trade numbers
 
-def test_calculate_size(order_size: OrderSize) -> None:
-    assert order_size.calculate_size(1) == pytest.approx(100000.0)
-    assert order_size.calculate_size(2) == pytest.approx(130000.0)
-    assert order_size.calculate_size(3) == pytest.approx(169000.0)
-    assert order_size.calculate_size(4) == pytest.approx(219700.0)
-    assert order_size.calculate_size(5) == pytest.approx(285610.0)
-    assert order_size.calculate_size(31) == pytest.approx(100000.0)
+    # Test initial trade
+    assert order_size_instance.calculate_size(1) == 100000
+
+    # Test a few trades to see if the scaling factor is applied correctly
+    assert order_size_instance.calculate_size(2) == 130000
+    assert order_size_instance.calculate_size(3) == 169000
+    assert order_size_instance.calculate_size(4) == 219700
+
+    # Test a trade after 30 trades (should reset back to the initial size)
+    assert order_size_instance.calculate_size(31) == 100000
+
+    # Add more test cases as needed
