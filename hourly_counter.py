4bf92a9929567703cd1ab49e04fd17505d921a23
Richard <richard.upwork.awj5R@protonmail.com>
2023-07-27 08:17:31 -0400

    Update hourly_counter.py

--- hourly_counter.py
+++ hourly_counter.py
@@ -1,3 +1,6 @@
+#region imports
+from AlgorithmImports import *
+#endregion
 
 class TimeFrameChecker:
     def __init__(self):
@@ -7,8 +10,17 @@ class TimeFrameChecker:
         # Simulate the passage of time with a 5-minute delay
         self.counter += 1
 
-    def is_one_hour_over(self) -> bool:
-        while self.counter < 12:  # 12 * 5 minutes = 1 hour
+    def is_one_hour_over(self, bar:str='5min') -> bool:
+        
+        _counter = 0
+        if bar =='5Min':
+            _counter = 12    
+        elif bar == '10Min':
+            _counter = 6
+        elif bar == '15Min':
+            _counter = 4
+        
+        while self.counter < _counter:  # 12 * 5 minutes = 1 hour
             self.increment_five_minute_bars()
             return False
-        return self.counter >= 12  # Return True if 1 hour is over, otherwise False
+        return self.counter >= _counter  # Return True if 1 hour is over, otherwise False
