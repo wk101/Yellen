d2bfc642edf2f7c0fb36f642d15bcdd35d2ee9dd
Richard <richard.upwork.awj5R@protonmail.com>
2023-07-27 08:35:57 -0400

    Update test_tp.py

--- tests/test_tp.py
+++ tests/test_tp.py
@@ -1,5 +1,5 @@
 import pytest
-from calculators.take_profit import TakeProfit
+from your_module import TakeProfit
 
 @pytest.fixture(scope="module")
 def tp():
@@ -7,31 +7,28 @@ def tp():
 
 @pytest.mark.parametrize("trade_num, price, trade_type, expected", [
     # Test short trade
-    (1, 100, "short", 80),  # 20% below
-    (2, 110, "short", 103),  # 70% between 100 and 110
-    (3, 115, "short", 106),  # 60% between 100 and 115
-    (4, 114, "short", 107.0),  # 50% between 100 and 114
-    (5, 120, "short", 111),  # 45% between 100 and 120
+    (1, 100, "short", 80),    # 20% below
+    (2, 110, "short", 103),   # 70% between 100 and 110
+    (3, 115, "short", 106),   # 60% between 100 and 115
+    (4, 114, "short", 107.0), # 50% between 100 and 114
+    (5, 120, "short", 111),   # 45% between 100 and 120
     # Test long trade
-    (1, 100, "long", 120),  # 20% above
-    (2, 90, "long", 97),  # 70% between 100 and 90 83 87 93
-    (3, 80, "long", 92)  # 60% between 100 and 80 68 72
+    (1, 100, "long", 120),    # 20% above
+    (2, 90, "long", 97),      # 70% between 100 and 90 83 87 93
+    (3, 80, "long", 92)       # 60% between 100 and 80 68 72
 ])
-def test_take_profit(tp, trade_num, price, trade_type, expected):
+def test_calculate_tp(tp, trade_num, price, trade_type, expected):
     assert tp.calculate_tp(trade_num, price, trade_type) == expected
 
-
-@pytest.mark.parametrize(
-    "trades, new_trade, error_message", [
-        ([(1, 100, "short"), (2, 110, "short")], (3, 102, "short"),
-         "The given price crosses a previous take profit level"),
-    ]
-)
-def test_price_crosses_previous_tp(trades, new_trade, error_message):
+def test_price_crosses_previous_tp():
     tp = TakeProfit()
-    for trade in trades:
-        tp.add_trade(*trade)
 
-    # Now attempt to add a trade that crosses the previous TP.
-    with pytest.raises(ValueError, match=error_message):
-        tp.add_trade(*new_trade)
+    # Add a trade that sets the first trade price (price=100, trade_type="short")
+    tp.calculate_tp(1, 100, "short")
+
+    # Add a second trade (price=110, trade_type="short")
+    tp.calculate_tp(2, 110, "short")
+
+    # Now attempt to add a third trade with price=102, which will cross the previous TP (103).
+    with pytest.raises(ValueError, match="The given price crosses a previous take profit level"):
+        tp.calculate_tp(3, 102, "short")
