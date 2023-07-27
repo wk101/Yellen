e4f0ad2a9405c51547143b18d794a06770369486
Richard <richard.upwork.awj5R@protonmail.com>
2023-07-27 10:56:49 -0400

    Update test_os.py

--- tests/test_os.py
+++ tests/test_os.py
@@ -1,5 +1,4 @@
-import pytest
-from order_size import OrderSize
+import os
 
 # Get the parent directory of the current script (assuming both files are in the same parent directory)
 parent_dir = os.path.dirname(os.path.abspath(__file__))
@@ -7,6 +6,8 @@ parent_dir = os.path.dirname(os.path.abspath(__file__))
 # Add the parent directory to the Python path
 sys.path.append(parent_dir)
 
+import pytest
+from order_size import OrderSize
 
 @pytest.fixture
 def order_size_instance():
